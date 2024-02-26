6364bec7773c34371d013177354a50d355b98fcc
kimoy catayas <kimoycatayas@gmail.com>
2024-02-26 12:51:27 +0800

    fix build

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,20 +10,20 @@ async function generate() {
     feed_url: 'https://kimoycatayas.com/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'blogs'))
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
   const allPosts = []
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'blogs', name)
+        path.join(__dirname, '..', 'pages', 'posts', name)
       )
       const frontmatter = matter(content)
 
       allPosts.push({
         title: frontmatter.data.title,
-        url: '/blogs/' + name.replace(/\.mdx?/, ''),
+        url: '/posts/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.tag.split(', '),
@@ -34,7 +34,7 @@ async function generate() {
 
   allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
   allPosts.forEach((post) => {
-      feed.item(post)
+    feed.item(post)
   })
   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
